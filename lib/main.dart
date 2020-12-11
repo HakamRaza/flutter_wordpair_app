ce8ff67fe491a21b08762b8863d451c06685f76f
HakamRaza <ismatcd@yahoo.com>
2020-12-11 11:26:46 +0800

    feat: using var words

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context){
+    final words = WordPair.random();
+
+
     return MaterialApp(
       theme: ThemeData(primaryColor: Colors.purple[900]),
       home: Scaffold(
         appBar: AppBar(title: Text('WordPair Generator')),
-        body: Center(child: Text('Hello World'))
+        body: Center(child: Text(words.asPascalCase))
       ));
   }
 }
