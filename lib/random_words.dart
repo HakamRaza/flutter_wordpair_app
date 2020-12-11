ab064e55840c85a08b8c352c645af34ba089a625
HakamRaza <ismatcd@yahoo.com>
2020-12-11 14:58:24 +0800

    fix: route to new screen, return list

--- lib/random_words.dart
+++ lib/random_words.dart
@@ -47,16 +47,55 @@ class RandomWordsState extends State<RandomWords>{
           if(alreadySaved){
             _savedWords.remove(pair); //removed from state
           } else {
-            _savedWords.add(pair); //add to state
+            _savedWords.add(pair); //add to state 
           }
         });
       },
       );
   }
 
+  
+  void _pushSaved() {
+    // a function, to route new page
+    Navigator.of(context).push(
+      //route
+      MaterialPageRoute(
+        builder: (BuildContext context) {
+          //create view list of words selected
+          final Iterable<ListTile> tiles =
+          _savedWords.map((WordPair pair) {
+            return ListTile(
+              title: Text(pair.asPascalCase, style: TextStyle(
+                fontSize: 16.0))
+            );
+          });
+
+          //create dividers for list words selected
+          final List<Widget> divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles
+          ).toList();
+
+          //return a render
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved Words')),
+              body:ListView(children: divided));
+        }));
+  }
+
+
   Widget build(BuildContext context){
     return Scaffold(
-      appBar: AppBar(title:Text('WordPair Generators')),
+      appBar: AppBar(
+        title:Text('WordPair Generators'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.list),
+            onPressed: _pushSaved, //call function pushSaved
+          )
+        ],
+        ),
       body:_buildList()
     );
   }
