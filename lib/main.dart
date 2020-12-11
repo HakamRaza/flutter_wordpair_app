c3e069fdf149b390565c8a951895e6607990959d
HakamRaza <ismatcd@yahoo.com>
2020-12-11 11:44:54 +0800

    feat: create class to return widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+// import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context){
-    final words = WordPair.random();
-
-
+    // final words = WordPair.random();
     return MaterialApp(
       theme: ThemeData(primaryColor: Colors.purple[900]),
-      home: Scaffold(
-        appBar: AppBar(title: Text('WordPair Generator')),
-        body: Center(child: Text(words.asPascalCase))
-      ));
+      home: RandomWords() //call class outside
+    );
+  }
+}
+
+//return class randomState createState
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords>{
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(title:Text('WordPair Generators')),
+      body:Text('Hello')
+    );
   }
 }
